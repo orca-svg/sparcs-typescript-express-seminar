d0a4e4c0c178cd7244ff46c103ec28e1e0066d06
static <kmc7468@naver.com>
2024-09-22 23:12:30 +0900

    [STAGE 2] 기본 라우팅

--- seminar/src/index.ts
+++ seminar/src/index.ts
@@ -1,10 +1,29 @@
 import express from "express";
+import cors, { CorsOptions } from "cors";
 
 const app = express();
 const port = 8080;
 
+const whitelist = ["http://localhost:3000"];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+} satisfies CorsOptions;
+
+app.use(cors(corsOptions));
+
 app.get("/", (req, res) => {
   res.send("Hello, world!");
+});
+
+app.get("/status", (req, res) => {
+  res.json({ isOnline: true });
 });
 
 app.listen(port, () => {
